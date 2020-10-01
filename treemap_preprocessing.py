6f20d3bd72507e638c660099751bddd52692eed9
TOAN <TOAN@DESKTOP-J80AGFG>
2020-10-01 13:22:04 +0700

    update treemap

--- treemap_preprocessing.py
+++ treemap_preprocessing.py
@@ -1,12 +1,19 @@
 import pandas as pd 
 import os 
+import unicodedata
+from unidecode import unidecode
 
-def danso():
+def convert_type(type_name):
+    if type_name == 'string':
+        return 'STRING'
+
+def danso(filename='danso1'):
     number_of_feature = 3
-    path = os.path.join(os.getcwd(), 'dataset', 'danso1.csv')
+    number_of_year = 3
+    path = os.path.join(os.getcwd(), 'dataset', '{}.csv'.format(filename))
     df_danso = pd.read_csv(path, skiprows=1, sep=';')
     df_danso = df_danso.dropna()
-    print(df_danso)
+    # print(df_danso)
     # print(df_danso.iloc[0,:])
     rows = df_danso.iloc[0,:].values
     rows = [x.strip() for x in rows]
@@ -20,17 +27,30 @@ def danso():
 
 
     cols = df_danso.columns.tolist()[1:]
+    cols = cols[::number_of_year]
     print(cols)
     results = []
+    datatypes = ['STRING', 'STRING', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT']
+    results.append(datatypes)
     for index, place in enumerate(places):
         vals = df_danso.iloc[index + 1,:].values
+        vals = [x if x != '..' else '0' for x in vals]
         # print(vals)
-        for i in range(number_of_feature):
-            results.append([place,rows[i], vals[i + 1 + 3 * 0], vals[i + 1 + 3 * 1], vals[i + 1 + 3 * 2], vals[i + 1 + 3 * 3], vals[1 + 3 * 4]])
-        print(results)    
-        break
-
+        # place = unicodedata.normalize('NFKD', place).encode('ascii', 'ignore')
+        place = unidecode(place)
+        for i in range(number_of_year):
+            try:
+                results.append([place.replace(' ', '_'), str(rows[i]), vals[i + 1 + 3 * 0], vals[i + 1 + 3 * 1], vals[i + 1 + 3 * 2], vals[i + 1 + 3 * 3], vals[i + 1 + 3 * 4]])
+            except:
+                results[0] = ['STRING', 'STRING', 'FLOAT', 'FLOAT', 'FLOAT']
+                results.append([place.replace(' ', '_'), str(rows[i]), vals[i + 1 + 3 * 0], vals[i + 1 + 3 * 1], vals[i + 1 + 3 * 2]])
+                    # print(results)    
+    labels = ['1', '2'] + cols
+    df = pd.DataFrame(results, columns=labels)
+    print(df.head())
+    
+    df.to_csv(os.path.join(os.getcwd(), 'dataset', '{}.tm3'.format(filename)), sep='\t', index=False)
 
 if __name__ == '__main__':
-    danso()
+    danso(filename='danso2')
     pass
